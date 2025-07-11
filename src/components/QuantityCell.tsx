0f6877ef9edd02061ff17fed9ff30a321dc594d5
adolfokrah <adolfokrah@gmail.com>
2025-07-11 19:55:48 +0200

    feat: added low stock alert

--- src/components/QuantityCell.tsx
+++ src/components/QuantityCell.tsx
@@ -1,18 +1,29 @@
 import { cn } from '@/lib/utils/twMerge'
 
-export default async function QuantityCell({ cellData, field }: { cellData?: number; field: any }) {
+export default async function QuantityCell({
+  cellData,
+  rowData,
+}: {
+  cellData?: number
+  rowData: any
+}) {
   if (!cellData) {
     return null
   }
 
+  const stockAlert = rowData?.stockAlert || rowData?.inventory?.stockAlert || null
+  const isLowStock = stockAlert && Number(cellData) < Number(stockAlert)
+
   return (
     <div
-      className={cn({
-        'tw:text-red-500': cellData < 0,
-        'tw:text-green-500': cellData >= 0,
+      className={cn('tw:flex tw:gap-3.5', {
+        'tw:text-red-500': (Number(cellData) || isLowStock) < 0,
+        'tw:text-green-500': Number(cellData) >= 0,
       })}
     >
       {cellData}
+
+      {isLowStock && <span className="tw-text-red-500 tw-text-xs">Stock is low!</span>}
     </div>
   )
 }
