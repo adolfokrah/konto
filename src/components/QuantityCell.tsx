d81c1f3b9e27562ecccf31372e222bb39a64f973
adolfokrah <adolfokrah@gmail.com>
2025-07-11 18:51:55 +0200

    fix: added tailwind css

--- src/components/QuantityCell.tsx
+++ src/components/QuantityCell.tsx
@@ -1,10 +1,18 @@
-import Link from 'next/link'
-import { Payload } from 'payload'
+import { cn } from '@/lib/utils/twMerge'
 
-export default async function BatchCell({ cellData, field }: { cellData?: number; field: any }) {
+export default async function QuantityCell({ cellData, field }: { cellData?: number; field: any }) {
   if (!cellData) {
     return null
   }
 
-  return <div style={{ color: cellData < 0 ? 'red' : 'green' }}>{cellData}</div>
+  return (
+    <div
+      className={cn({
+        'tw:text-red-500': cellData < 0,
+        'tw:text-green-500': cellData >= 0,
+      })}
+    >
+      {cellData}
+    </div>
+  )
 }
