14f6ff96c8771ec97ae52a1bea8a685b38d561e2
adolfokrah <adolfokrah@gmail.com>
2025-07-16 19:09:04 +0200

    fix: test

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
 
 import config from '@/payload.config'
 
@@ -23,12 +22,7 @@ export default async function HomePage() {
       <div className="content">
         <picture>
           <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
+          <h1>Konto</h1>
         </picture>
         {!user && <h1>Welcome to your new project.</h1>}
         {user && <h1>Welcome back, {user.email}</h1>}
