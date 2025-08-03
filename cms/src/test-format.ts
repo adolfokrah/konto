e48b4f78ab4d3e6c5a90218f679680896d6d7571
adolfokrah <adolfokrah@gmail.com>
2025-08-03 13:45:03 +0200

    test: add pre-commit hook with test files

---
+++ cms/src/test-format.ts
@@ -0,0 +1,5 @@
+// Test file with intentional formatting issues
+export const testFunction = () => {
+  console.log('This should be formatted')
+  return 'test'
+}
---
+++ cms/test-precommit.md
@@ -0,0 +1,3 @@
+# Test file for pre-commit hook
+
+This file tests the pre-commit hook functionality.
