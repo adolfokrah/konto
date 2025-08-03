caca6f02abb5654a22824b0ebd28d7303aaff062
adolfokrah <adolfokrah@gmail.com>
2025-08-03 13:45:46 +0200

    fix: removed test files

--- cms/src/test-format.ts
+++
@@ -1,5 +0,0 @@
-// Test file with intentional formatting issues
-export const testFunction = () => {
-  console.log('This should be formatted')
-  return 'test'
-}
--- cms/test-precommit.md
+++
@@ -1,3 +0,0 @@
-# Test file for pre-commit hook
-
-This file tests the pre-commit hook functionality.
