d80091fc73eab046aaf0e0e164924b423a161fc1
adolfokrah <adolfokrah@gmail.com>
2025-08-10 12:46:55 +0200

    feat: finalized onboarding feature

--- mobile_app/lib/features/onboarding/prensentation/widgets/onboarding_slider.dart
+++ mobile_app/lib/features/onboarding/prensentation/widgets/onboarding_slider.dart
@@ -1,32 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:konto/core/constants/onboarding_data.dart';
 
-class OnBoardingSlider extends StatelessWidget {
-  final int currentPage; // Placeholder for current page index
+class OnBoardingSlider extends StatefulWidget {
+  final int currentPage;
   final Function(int)? onPageChanged;
-  OnBoardingSlider({super.key, required this.currentPage, this.onPageChanged});
-  final PageController _pageController = PageController();
+  
+  const OnBoardingSlider({
+    super.key, 
+    required this.currentPage, 
+    this.onPageChanged,
+  });
+
+  @override
+  State<OnBoardingSlider> createState() => _OnBoardingSliderState();
+}
+
+class _OnBoardingSliderState extends State<OnBoardingSlider> {
+  late PageController _pageController;
+  bool _isAnimating = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: widget.currentPage);
+  }
+
+  @override
+  void didUpdateWidget(OnBoardingSlider oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    
+    // If currentPage changed externally (e.g., from button click), animate to that page
+    if (widget.currentPage != oldWidget.currentPage && !_isAnimating) {
+      _animateToPage(widget.currentPage);
+    }
+  }
+
+  void _animateToPage(int page) {
+    _isAnimating = true;
+    _pageController.animateToPage(
+      page,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    ).then((_) {
+      _isAnimating = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-     return Expanded(
-        child: PageView.builder(
-          controller: _pageController,
-          onPageChanged: (index) {
-            onPageChanged?.call(index); // Notify parent about page change
-          },
-          itemCount: onBoardingData.length,
-          itemBuilder: (context, index) {
-            return Center(
-              child: Image.asset(
-                onBoardingData[index].illustration,
-                width: 700,
-                height: 700,
-                fit: BoxFit.contain,
-              ),
-            );
-          },
-        ),
-      );
+    return Expanded(
+      child: PageView.builder(
+        controller: _pageController,
+        onPageChanged: (index) {
+          if (!_isAnimating) {
+            widget.onPageChanged?.call(index);
+          }
+        },
+        itemCount: onBoardingData.length,
+        itemBuilder: (context, index) {
+          return Center(
+            child: Image.asset(
+              onBoardingData[index].illustration,
+              width: 700,
+              height: 700,
+              fit: BoxFit.contain,
+            ),
+          );
+        },
+      ),
+    );
   }
 }
