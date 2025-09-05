f62c9a782746a0c10d49184269394adae11875a8
adolfokrah <adolfokrah@gmail.com>
2025-09-05 17:30:36 +0200

    update

--- cms/src/payload-types.ts
+++ cms/src/payload-types.ts
@@ -188,7 +188,7 @@ export interface Page {
             /**
              * Choose how the link should be rendered.
              */
-            appearance?: ('default' | 'outline') | null;
+            appearance?: ('default' | 'outline-solid') | null;
           };
           id?: string | null;
         }[]
@@ -460,7 +460,7 @@ export interface CallToActionBlock {
           /**
            * Choose how the link should be rendered.
            */
-          appearance?: ('default' | 'outline') | null;
+          appearance?: ('default' | 'outline-solid') | null;
         };
         id?: string | null;
       }[]
@@ -510,7 +510,7 @@ export interface ContentBlock {
           /**
            * Choose how the link should be rendered.
            */
-          appearance?: ('default' | 'outline') | null;
+          appearance?: ('default' | 'outline-solid') | null;
         };
         id?: string | null;
       }[]
