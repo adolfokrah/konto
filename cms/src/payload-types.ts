05a49bee0ca24a28fe1c9d644a536e86b7d198fe
adolfokrah <adolfokrah@gmail.com>
2025-09-19 17:51:41 +0200

    fix: fixed typed issues

--- cms/src/payload-types.ts
+++ cms/src/payload-types.ts
@@ -280,7 +280,6 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
-  _key?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -294,7 +293,6 @@ export interface Media {
   focalY?: number | null;
   sizes?: {
     thumbnail?: {
-      _key?: string | null;
       url?: string | null;
       width?: number | null;
       height?: number | null;
@@ -303,7 +301,6 @@ export interface Media {
       filename?: string | null;
     };
     square?: {
-      _key?: string | null;
       url?: string | null;
       width?: number | null;
       height?: number | null;
@@ -312,7 +309,6 @@ export interface Media {
       filename?: string | null;
     };
     small?: {
-      _key?: string | null;
       url?: string | null;
       width?: number | null;
       height?: number | null;
@@ -321,7 +317,6 @@ export interface Media {
       filename?: string | null;
     };
     medium?: {
-      _key?: string | null;
       url?: string | null;
       width?: number | null;
       height?: number | null;
@@ -330,7 +325,6 @@ export interface Media {
       filename?: string | null;
     };
     large?: {
-      _key?: string | null;
       url?: string | null;
       width?: number | null;
       height?: number | null;
@@ -339,7 +333,6 @@ export interface Media {
       filename?: string | null;
     };
     xlarge?: {
-      _key?: string | null;
       url?: string | null;
       width?: number | null;
       height?: number | null;
@@ -348,7 +341,6 @@ export interface Media {
       filename?: string | null;
     };
     og?: {
-      _key?: string | null;
       url?: string | null;
       width?: number | null;
       height?: number | null;
@@ -1366,7 +1358,6 @@ export interface PostsSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
-  _key?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
@@ -1384,7 +1375,6 @@ export interface MediaSelect<T extends boolean = true> {
         thumbnail?:
           | T
           | {
-              _key?: T;
               url?: T;
               width?: T;
               height?: T;
@@ -1395,7 +1385,6 @@ export interface MediaSelect<T extends boolean = true> {
         square?:
           | T
           | {
-              _key?: T;
               url?: T;
               width?: T;
               height?: T;
@@ -1406,7 +1395,6 @@ export interface MediaSelect<T extends boolean = true> {
         small?:
           | T
           | {
-              _key?: T;
               url?: T;
               width?: T;
               height?: T;
@@ -1417,7 +1405,6 @@ export interface MediaSelect<T extends boolean = true> {
         medium?:
           | T
           | {
-              _key?: T;
               url?: T;
               width?: T;
               height?: T;
@@ -1428,7 +1415,6 @@ export interface MediaSelect<T extends boolean = true> {
         large?:
           | T
           | {
-              _key?: T;
               url?: T;
               width?: T;
               height?: T;
@@ -1439,7 +1425,6 @@ export interface MediaSelect<T extends boolean = true> {
         xlarge?:
           | T
           | {
-              _key?: T;
               url?: T;
               width?: T;
               height?: T;
@@ -1450,7 +1435,6 @@ export interface MediaSelect<T extends boolean = true> {
         og?:
           | T
           | {
-              _key?: T;
               url?: T;
               width?: T;
               height?: T;
