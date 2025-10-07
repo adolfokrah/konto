c315051417fe673a0ee44200f1fa9545966c365f
adolfokrah <adolfokrah@gmail.com>
2025-10-07 09:15:33 +0200

    fix: updates

--- cms/src/app/email/page.tsx
+++ cms/src/app/email/page.tsx
@@ -1,10 +1,15 @@
-'use client'
-
+import { notFound } from 'next/navigation'
 import KycVerified from '@/components/emailTemplates/kycVerified'
 import Welcome from '@/components/emailTemplates/Welcome'
 import ContributionsRepport from '@/components/emailTemplates/contributionReport'
 import AccountDeletion from '@/components/emailTemplates/accountDeletion'
+
 export default function Email() {
+  // Only allow this route in development
+  if (process.env.NODE_ENV === 'production') {
+    notFound()
+  }
+
   return (
     <div>
       {/* <KycVerified fullname="Ben" /> */}
